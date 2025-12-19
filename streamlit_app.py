5e22d9b771d2f74336b219feb7795ea7576a99c6
yalvarado <yago.a@7peakssoftware.com>
2025-12-19 11:14:44 +0700

    moved api call within the for loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -43,8 +45,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
     
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
 
 
 
